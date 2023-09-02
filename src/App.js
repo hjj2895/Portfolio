dbd7a371e6949099ffe6b23beec2b9a976ca8c9e
peter ji <hyeongjun9@gmail.com>
2023-09-01 17:16:40 -0700

    changes made

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './routes/Home';
 import About from './routes/About';
 import Projects from './routes/Projects';
 import Contact from './routes/Contact';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 
@@ -14,12 +14,14 @@ function App() {
   return (
     <>
     <Navbar />
+    <Router basename="/tothepoint_login">
      <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/projects" element={<Projects/>} />
       <Route path="/contact" element={<Contact/>} />
      </Routes>
+     </Router>
     </>
   );
 }
